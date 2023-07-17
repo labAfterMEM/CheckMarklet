c43cc680a8dffea12f44dba861c08614563671fb
kapy99 <kapibara2381@gmail.com>
2023-07-17 15:31:28 +0900

    Create template folder

---
+++ src/components/_Template/Template.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export const Template:React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
---
+++ src/pages/home.tsx
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+export const Home:React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    </>
+  )
+}
